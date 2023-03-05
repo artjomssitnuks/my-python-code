0cec6d88e970d255f89be20dc5f6d2fd82c221b4
artjomssitnuks <125254369+artjomssitnuks@users.noreply.github.com>
2023-03-05 13:26:57 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,61 @@
-# python3
+"""
+This module calculates the height of a tree given its parent array.
+"""
+class Node:
+    """
+    A class representing a node in a binary tree.
+    value -- the value of the node.
+    children -- a list of the node's children.
+    """
+    def __init__(self, value):
+        self.value = value
+        self.children = []
 
-import sys
-import threading
-import numpy
+def compute_height(node):
+    """
+    Compute the height of a binary tree with the given root node.
+    node -- the root node of the binary tree.
+    Returns the height of the binary tree.
+    """
+    if not node.children:
+        return 1
+    else:
+        heights = []
+        for child in node.children:
+            heights.append(compute_height(child))
+        return 1 + max(heights)
 
+input_source = input()
+input_source = input_source.upper()
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+if input_source == "I":
+    n = int(input())
+    parent = list(map(int, input().split()))
+elif input_source == "F":
+    filename = input()
+    if "a" in filename.lower():
+        print("Error: File name cannot contain the letter 'a'")
+    else:
+        if input_source == 'i':
+            n = int(input())
+            parent = list(map(int, input().split()))
+        elif filename.lower() == 'f':
+            file = input("Enter file path: ")
+            with open(file, "r", encoding="utf-8") as f:
+                n = int(f.readline().strip())
+                parent = list(map(int, f.readline().strip().split()))
+        else:
+            print("Invalid input. Please enter 'i' for keyboard input or 'f' for file input.")
 
+if input_source in ["I", "F"]:
+    nodes = []
+    for i in range(n):
+        nodes.append(Node(i))
 
-def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    for i in range(n):
+        if parent[i] == -1:
+            root = nodes[i]
+        else:
+            nodes[parent[i]].children.append(nodes[i])
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+    print(compute_height(root))
