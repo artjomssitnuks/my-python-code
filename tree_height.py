9caa1318a1c7df80f5bb11626ba1b6b15c08d572
artjomssitnuks <125254369+artjomssitnuks@users.noreply.github.com>
2023-11-19 15:27:37 +0200

    random.py

--- tree_height.py
+++ tree_height.py
@@ -1,49 +1,13 @@
-import os.path
+import random
 
-class Node:
-    
-    def __init__(self, value):
-        self.value = value
-        self.children = []
+def generate_random_number():
+    return random.randint(1, 100)
 
-def compute_height(root):
-    height = 1
-    queue = [root]
-    while queue:
-        level_size = len(queue)
-        for i in range(level_size):
-            node = queue.pop(0)
-            for child in node.children:
-                queue.append(child)
-        if queue:
-            height += 1
-    return height
+def main():
+    random_number = generate_random_number()
+    print("Random number:", random_number)
 
-first_input = input()
+if __name__ == "__main__":
+    main()
 
-if first_input.startswith("I"):
-    second_input = input()
-    n = int(second_input)
-    parent = list(map(int, input().split()))
-elif first_input.startswith("F"):
-    filename = str(input())
-    if 'a' in filename:
-        print("Invalid filename")
-        exit()
-    filename = "test/" + filename
-    with open(filename, 'r') as f:
-        n = int(f.readline())
-        parent = [int(x) for x in f.readline().split()]
-
-nodes = []
-for i in range(n):
-    nodes.append(Node(i))
-
-for i in range(n):
-    if parent[i] == -1:
-        root = nodes[i]
-    else:
-        nodes[parent[i]].children.append(nodes[i])
-
-print(compute_height(root))
 print('Goodbye')
